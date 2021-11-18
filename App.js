01a010074c2251750d3d5d916b6778bd5f6a8920
Daniel Sanchez <sanchezd0528@gmail.com>
2021-11-17 21:36:07 -0500

    correcciones

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet} from 'react-native'
 import Suma from './src/components/Suma.js'
 import ValidarNumeros from './src/components/ValidarNumeros.js'
 
-import Componente from './src/components/Componente.js'
+
 export default class App extends Component {
   render() {
     return (
-      <Componente/>
+      <Suma/>
     ) 
   }
 }
